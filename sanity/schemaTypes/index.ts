e3e559d09ee78b72f2f169553b8e552f9e20d9aa
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-16 18:38:17 +0600

    Add Sanity schema index

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import { event } from './event';
+import { galleryItem } from './galleryItem';
+import { partner } from './partner';
+import { person } from './person';
+import { post } from './post';
+
+export const schemaTypes = [event, person, partner, post, galleryItem];

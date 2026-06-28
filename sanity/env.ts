45814ca995382cba82dbfec3d42b2a0c71e64b95
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-28 16:09:48 +0600

    Connect Sanity project id

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,3 +1,3 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-01';
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'demo0000';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'gs1460rq';

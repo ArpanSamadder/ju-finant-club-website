60ff11e00cfd26914b608086a184e2b11752656b
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-28 15:20:37 +0600

    Register legacy event schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
-import { event } from './event';
-import { galleryItem } from './galleryItem';
-import { partner } from './partner';
-import { person } from './person';
-import { post } from './post';
+import {event} from './event';
+import {galleryItem} from './galleryItem';
+import {legacyEvent} from './legacyEvent';
+import {partner} from './partner';
+import {person} from './person';
+import {post} from './post';
 
-export const schemaTypes = [event, person, partner, post, galleryItem];
+export const schemaTypes = [event, legacyEvent, person, partner, post, galleryItem];

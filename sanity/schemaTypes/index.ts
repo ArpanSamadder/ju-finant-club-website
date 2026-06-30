44186fcce5921fb4faaefed6424be3a5f66b3e03
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-30 20:20:41 +0600

    Register voice of FinAnt schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,5 +4,6 @@ import {legacyEvent} from './legacyEvent';
 import {partner} from './partner';
 import {person} from './person';
 import {post} from './post';
+import {voiceOfFinant} from './voiceOfFinant';
 
-export const schemaTypes = [event, legacyEvent, person, partner, post, galleryItem];
+export const schemaTypes = [event, legacyEvent, person, partner, post, galleryItem, voiceOfFinant];
